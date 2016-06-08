a11366b7af07c96db74f4fd33af8d08aebb0be3f
tyler_wood <tyler.wood@airbnb.com>
2016-06-07 22:53:16 -0700

    updating the thread

--- water_post_template.py
+++ water_post_template.py
@@ -3,15 +3,29 @@
 import config 
 import requests
 import datetime
+import pytz
 
 from datetime import datetime as dt
+from pytz import timezone
 
 # Date time set up
 date_format = '%H:%M:%S PST'
 title_format = '%Y-%m-%d'
-now = dt.now()
-now_formatted = now.strftime(date_format)
-end_time = now + datetime.timedelta(0, 12*3600)
+PST = timezone('US/Pacific')
+
+utc_now = dt.now(tz=pytz.utc)
+now_pst = utc_now.astimezone(PST)
+
+def ord(n):
+    if 4<=n%100<=20:
+        return str(n)+"th"
+    else:
+        return str(n)+{1:"st",2:"nd",3:"rd"}.get(n%10,"th")
+
+ord_date = ord(int(now_pst.strftime('%-d')))
+
+now_formatted = now_pst.strftime(date_format)
+end_time = now_pst + datetime.timedelta(0, 12*3600)
 end_time_formatted = end_time.strftime(date_format)
 
 # Weather update
@@ -19,26 +33,36 @@ api = config.weather_api
 url = 'http://api.openweathermap.org/data/2.5/weather?zip={ZIP},us&APPID={APIKEY}'
 url = url.format(ZIP=config.zipcode, APIKEY=api)
 r = requests.get(url)
-description = r.json()['weather'][0]['description']
+description = r.json()['weather'][0]['main']
 
 title = """
-Do you want to water the plant today? {0}
+Today is {0}, {1} {2}. Do you want to water the plant today? 
 """
-title = title.format(now.strftime(title_format))
+
+title = title.format(now_pst.strftime('%A'),
+                     now_pst.strftime('%B'), 
+                     ord_date) 
+                    
 body = """
-Hello! 
+Hello, and thanks for taking care of my plant! 
 
-It is **{0}** in California.
-The weather report today calls for: *{1}*.
+It is **{0}** in California and the weather report today calls for: *{1}*.
 
-This thread will close in **12** hours, at which time all top-level comments
-will be assessed as votes in favor of watering the plant, or not. 
 
-**VOTING**
-Comment 'Yes' or 'No'
+If you think that the plant should be watered today, please comment `yes` on
+this post. If you think that plant should not be watered today, please comment
+`no` below. 
 
-The decision will be made at {2}, and this thread will be updated, and locked
-at that time.
+
+When this post is 12 hours old, at **{2}**, this post will be locked.
+u/takecareofmyplant will tally all the `yes` or `no` votes in top level comments
+on this thread. 
+
+If `yes` is the majority, u/takecareofmyplant will turn on the
+pump and water the plant. If not, we will check in again tomorrow!
+
+
+**To help take care of my plant, please vote below!**
 """
 
 body = body.format(now_formatted, description, end_time_formatted)
