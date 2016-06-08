219a69a091df0a3dcf215725c103f290b1e1e73d
tyler_wood <tyler.wood@airbnb.com>
2016-06-07 22:58:59 -0700

    rpi output script to call

---
+++ gpio_out.py
@@ -0,0 +1,15 @@
+# Must run as root on rpi
+
+import RPi.GPIO as GPIO
+import time
+
+OUTPUT_PIN = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+
+GPIO.output(OUTPUT_PIN, True)
+time.sleep(5)
+GPIO.output(OUTPUT_PIN, False)
+
+
