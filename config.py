c4b3789e641ac289fec3ae541fbc298027ff5899
tyler_wood <tyler.wood@airbnb.com>
2016-06-15 21:46:49 -0700

    config to pi

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+from getpass import getuser
+
+import praw
+
+from secret import user_agent, password, weather_api, zipcode, test_sub
+
+# PRAW
+user_name = 'takecareofmyplant'
+subreddit = 'takecareofmyplant'
+
+def getReddit():
+    r = praw.Reddit(user_agent=user_agent)
+    r.login(user_name, password)
+    return r
+
+def getSubReddit(Reddit, test=True):
+    sub = test_sub if test else subreddit
+    sr = Reddit.get_subreddit(sub)
+    return sr
+
+# Files
+def pathPrefix():
+    pi = '/home/pi/pleasetakecareofmyplant/'
+    tyler = '/Users/tyler_wood/personal/pleasetakecareofmyplant/'
+    user = getuser()
+    path = tyler if user == 'tyler_wood' else pi
+    return path
+
+# Time 
+
+
+
+
