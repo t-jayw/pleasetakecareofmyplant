54a081325b24b6d1adc5037e2f923a47b6b0837b
tyler <tylerjaywood@gmail.com>
2016-06-29 04:22:21 +0000

    screen caps

--- water_thread_wrapup.py
+++ water_thread_wrapup.py
@@ -95,7 +95,7 @@ if total > 0:
       f.write('1')
       f.close()
       time.sleep(600)
-      g.on_off(20)
+      g.on_off(15)
 else:
     with open(path+'topup.txt', 'w') as f:
       f.write('0') 
