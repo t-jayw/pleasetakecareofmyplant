649c834dcbb6dec28f42111665b8f4bf5e83e881
tyler <tylerjaywood@gmail.com>
2016-07-02 07:47:25 +0000

    moisture monitor started

---
+++ moisture_monitor/example.py
@@ -0,0 +1,39 @@
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+# Software SPI configuration:
+CLK  = 22 
+MISO = 27 
+MOSI = 17 
+CS   = 4  
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Hardware SPI configuration:
+# SPI_PORT   = 0
+# SPI_DEVICE = 0
+# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+
+print('Reading MCP3008 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+print('-' * 57)
+# Main program loop.
+while True:
+    # Read all the ADC channel values in a list.
+    values = [0]*8
+    for i in range(8):
+        # The read_adc function will get the value of the specified channel (0-7).
+        values[i] = mcp.read_adc(i)
+    # Print the ADC values.
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    # Pause for half a second.
+    time.sleep(0.5)
---
+++ moisture_monitor/moisture_gpio.py
@@ -0,0 +1,15 @@
+# Must run as root on rpi
+
+import RPi.GPIO as GPIO
+import time
+
+OUTPUT_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+
+def on_off(t=5):
+    GPIO.output(OUTPUT_PIN, True)
+    time.sleep(t)
+    GPIO.output(OUTPUT_PIN, False)
+    GPIO.cleanup()
