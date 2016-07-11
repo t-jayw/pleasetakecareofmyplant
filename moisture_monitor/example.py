c0b2143f03070d471aec81afcb38493d5d26375d
tyler <tylerjaywood@gmail.com>
2016-07-11 05:56:14 +0000

    some photos

--- moisture_monitor/example.py
+++ moisture_monitor/example.py
@@ -3,6 +3,7 @@
 # Author: Tony DiCola
 # License: Public Domain
 import time
+import requests
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
@@ -26,14 +27,20 @@ print('Reading MCP3008 values, press Ctrl-C to quit...')
 # Print nice channel column headers.
 print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
 print('-' * 57)
+
+output = ""
 # Main program loop.
-while True:
-    # Read all the ADC channel values in a list.
+for x in range(0,10):    
+  # Read all the ADC channel values in a list.
     values = [0]*8
     for i in range(8):
         # The read_adc function will get the value of the specified channel (0-7).
         values[i] = mcp.read_adc(i)
     # Print the ADC values.
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-    # Pause for half a second.
+    foo=('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    output += foo 
     time.sleep(0.5)
+
+url = 'http://www.pleasetakecareofmyplant.com/datatest.php'
+payload = {'foo':output}
+r = requests.post(url, data=payload)
