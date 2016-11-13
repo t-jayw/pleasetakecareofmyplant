e76362f00aaeefe3a19ab1666d8f5ea9525dc5a4
tyler <tylerjaywood@gmail.com>
2016-11-13 01:24:46 -0100

    fixing moisture monitor power to only switch on when reading

--- moisture_monitor/moisture_gpio.py
+++ moisture_monitor/moisture_gpio.py
@@ -3,7 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 
-OUTPUT_PIN = 18
+OUTPUT_PIN = 26
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(OUTPUT_PIN, GPIO.OUT)
@@ -11,5 +11,12 @@ GPIO.setup(OUTPUT_PIN, GPIO.OUT)
 def on_off(t=5):
     GPIO.output(OUTPUT_PIN, True)
     time.sleep(t)
+    GPIO.output(OUTPUT_PIN, False)
+    GPIO.cleanup()
+
+def on():
+    GPIO.output(OUTPUT_PIN, True)
+
+def off():
     GPIO.output(OUTPUT_PIN, False)
     GPIO.cleanup()
